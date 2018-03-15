1a000e5d12e389e8181a2f3e922e8e0afcc578a6
Ryanak <ranaka541@gmail.com>
2018-03-15 23:39:20 +0900

    Ringonize

--- src/commands/address.js
+++ src/commands/address.js
@@ -1,15 +1,14 @@
 const { OOPS_TEXT } = require('../messages')
 
-const ADDRESS_TEXT = 'You can send dogecoin to this address : '
+const ADDRESS_TEXT = 'You can send Ringo to this address : '
 
-function address (message, dogecoinNode) {
+function address (message, coind) {
   var account = message.author.id
 
   // Will create a new account if doesn't exist... ? Should we allow this ?
   // Yes
-  dogecoinNode.getAccountAddress(account, function (err, address) {
+  coind.getAccountAddress(account, function (err, address) {
     if (err) {
-      console.log(err)
       message.channel.send(OOPS_TEXT)
       return
     }
--- src/commands/balance.js
+++ src/commands/balance.js
@@ -1,24 +1,18 @@
 const { OOPS_TEXT } = require('../messages')
 const { rateDogeEur } = require('../requests')
 
-const BALANCE_TEXT = 'Wow. Balance : '
+const BALANCE_TEXT = 'Balance: '
 
 function balance (message, dogecoinNode) {
   var account = message.author.id
 
   dogecoinNode.getBalance(account, function (err, balance) {
     if (err) {
-      console.log(err)
       message.channel.send(OOPS_TEXT)
       return
     }
-
-    rateDogeEur().then((rate) => {
-      message.channel.send(BALANCE_TEXT + (balance).toFixed(2) + ' DOGE ( ' + (balance * rate).toFixed(2) + ' EUR )')
-    }).catch((error) => {
-      message.channel.send(error)
-    })
-  })
+    message.channel.send(BALANCE_TEXT + balance +" RIN")
+})
 }
 
 module.exports = balance
