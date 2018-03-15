8b156a260dc19f7717d11c046472e881c819b36b
Ryanak <ranaka541@gmail.com>
2018-03-15 21:11:34 +0900

    update emotip

--- src/emotip.js
+++ src/emotip.js
@@ -1,40 +1,54 @@
 
 const emotiplist = require('../emotiplist.json')
 
-const { OOPS_TEXT } = require('./messages')
 const TIP_TEXT = 'Wow. Much coins.'
 const PROPER_AMOUNT_TEXT = 'You need provide a proper amount to be send.'
-const NO_COMMA_TEXT = 'Please avoid "," in your amount and use "."'
-const NEED_USER_TEXT = 'Need a user as a third argument'
 const NOT_ENOUGH_FUNDS = 'Not enough funds for this transfer. Please add some dogecoins.'
 
 function emotip(reaction, user, coind) {
+    //絵文字IDを表示
+    if (reaction.message.content.startsWith("EMOJI_ID")) {
+        sendDM(user, reaction.emoji.id)
+    }
+
     if (emotiplist[reaction.emoji.id]) {
         let fromAccount = user.id
         let toAccount = reaction.message.author.id
-        let amount = emotiplist[reaction.emoji.id]
-        if(fromAccount==toAccount||reaction.message.author.bot)return
-
+        let amount = parseInt(emotiplist[reaction.emoji.id])
+        if (fromAccount == toAccount || reaction.message.author.bot) return
+        console.log(fromAccount + ":" + toAccount + "+" + amount)
         coind.getBalance(fromAccount, function (err, balance) {
             if (err) {
-                console.log(err)
+                console.error(err)
+                reaction.remove()
                 return
             }
-
+            console.log(balance + "+" + amount)
             // We don't have enough funds...
             if (balance - amount <= 0) {
+                reaction.remove()
+                sendDM(user, NOT_ENOUGH_FUNDS)
                 return
             }
 
             coind.move(fromAccount, toAccount, amount, function (err, result) {
                 if (err) {
-                    console.log(err)
-                    reaction.message.channel.send(OOPS_TEXT)
+                    console.error(err)
+                    reaction.remove()
                     return
                 }
-                reaction.message.reply("<="+user.name+amount)
+                console.log(fromAccount + ">" + fromAccount + "=" + amount)
+                sendDM(reaction.message.author, user.username + "さんから" + amount)
+
             })
         })
+
     }
+}
+
+function sendDM(to, message) {
+    to.createDM().then(function (DM) {
+        DM.send(message)
+    })
 }
 module.exports = emotip
