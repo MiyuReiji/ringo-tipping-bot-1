cf9bf013aa5ca2821f18757f5e14521f46f391e5
Reiji Miyu <37491987+MiyuReiji@users.noreply.github.com>
2018-03-18 12:35:20 +0900

    Replace with Japanese help message

--- src/commands/help.js
+++ src/commands/help.js
@@ -1,10 +1,13 @@
 const HELP_TEXT = '```Command list : \n \
-  - **help** : Get help ! \n \
-  - **tip** : Tip someone some Ringo \n \
-  - **balance** : Get your balance \n \
-  - **address** : Show an used wallet so you can refill your wallet \n \
-  - **withdraw** : Move coins to an external address \n \
-  - **qrcode** : Show your qrcode to receive much money \n \
+  - **help** : コマンド一覧を表示します \n \
+  - **tip** : Ringoを送信します。tip <数量> <送信先>形式です  \n \
+  - **balance** : 現在のRingo残高を表示します。 \n \
+  - **address** : 預入れ用のRingoアドレスを表示します \n \
+  - **withdraw** : Ringoを引出します。withdeaw <数量> <Ringoアドレス>形式です \n \
+  - **qrcode** : ＱＲコードを表示します。 \n \
+  - 注意 : withdrawには引出手数料 0.0001Rinが必要です。 \n \
+  - tipについて : 複数人へ送信することはできません。 \n \
+  - 預入用アドレスは時々変わりますので、都度確認してください。\n \
 ```'
 
 function help(message) {
